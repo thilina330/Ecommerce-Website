55a49f65b700d034899cc574c6d75743eaafb367
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-06-18 10:07:56 +0530

    create appjs file

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Navitems from './components/Navitems'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navitems/>
+      <Outlet/>
+    
+    </>
+  )
+}
+
+export default App
