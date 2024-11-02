b095c4ecaba19d739065f403943b1158bf3c7f3b
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-11-02 13:42:41 +0530

    create aboutUs part

---
+++ src/home/AboutUs.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const subTitle = " Why Choose Us";
+const title = "Become a Marchant";
+const desc = "Take courses on your any device with our app & learn about business what you want. just download & install & start to learn"
+const btnText = "Apply Now";
+
+const countList = [
+
+    {
+        iconName: "icofont-users-alt-4",
+        count: 12600,
+        text: 'Marchant Enrolled'
+    },
+
+    {
+        iconName: "icofont-graduate-alt",
+        count: 30,
+        text: 'Certified Courses'
+    },
+
+    {
+        iconName: "icofont-notification",
+        count: 100,
+        text: 'Rewards and GitCards'
+    }
+]
+
+const AboutUs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AboutUs
