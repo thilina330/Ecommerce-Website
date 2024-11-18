f7de6b2667e771f116ed0c222b40e3c2ede1db12
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-11-19 02:22:19 +0530

    create about us section

--- src/home/AboutUs.jsx
+++ src/home/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CountUp from 'react-countup';
+import { Link } from 'react-router-dom';
 
 const subTitle = "Why Choose Us";
 const title = "Become a Merchant";
@@ -30,7 +31,7 @@ const AboutUs = () => {
       <div className="container">
         <div className="section-wrapper">
           
-          <div className="row">
+          <div className="row g-4 justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-xl-3">
             <div className="col">
               {countList.map((val, i) => (
                 <div key={i} className="count-item">
@@ -39,11 +40,28 @@ const AboutUs = () => {
                       <i className={val.iconName}></i>
                     </div>
                     <div className="count-content">
-                      <h2><span><CountUp end={val.count}/></span></h2>
+                      <h2>
+                       <span><CountUp end={val.count}/></span>
+                       <span>+</span>
+                      </h2>
+                      <p>{val.text}</p>
                     </div>
                   </div>
                 </div>
               ))}
+            </div>
+            <div className='col'>
+              <div className='instructor-content'>
+                <span className='subtitle'>{subTitle}</span>
+                <h2 className='title'>{title}</h2>
+                <p>{desc}</p>
+                <Link to="/sign-up" className='lab-btn'>{btnText}</Link>
+              </div>
+            </div>
+            <div className="col">
+              <div className="instructor-thumb">
+                <img src="/src/assets/images/instructor/01.png" alt="instructor" />
+              </div>
             </div>
           </div>
         </div>
