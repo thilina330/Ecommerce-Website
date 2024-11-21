4328f1e1e2b356afb523cb0bcd121346d30ce003
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-11-21 08:40:43 +0530

    update blog page

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageHeader from '../components/PageHeader'
 
 const Blog = () => {
   return (
     <div>
-      
+      <PageHeader title="Blog Page" curPage="Blogs"/>
     </div>
   )
 }
