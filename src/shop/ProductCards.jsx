2328c320ec08711c6e603a34cac7ad897296b547
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-11-21 08:41:25 +0530

    create product card

---
+++ src/shop/ProductCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductCards = ({ GridList, products }) => {
+    console.log(products);
+    
+  return (
+    <div>
+      h
+    </div>
+  )
+}
+
+export default ProductCards
